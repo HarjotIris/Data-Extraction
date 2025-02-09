93394a554cf66c8478081506d43dcb064359252e
HarjotIris <harjotsingh22nov@gmail.com>
2025-02-09 19:01:05 +0530

    BBC Latest Headlines scraper project

---
+++ BBC News Headlines/News/Latin America/latin america latest updates.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+data = {'Headline': [], 'Description': []}
+
+payload = {'api_key': '397a4c2c951cdb70873ad43ee651270c', 'url':'https://www.bbc.com/news/world/latin_america' }
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+}
+
+# Send request through ScraperAPI with headers
+r = requests.get(
+    "http://api.scraperapi.com/",
+    params= payload,
+    headers=headers
+)
+
+soup = BeautifulSoup(r.text, "html.parser")
+# print(soup.prettify())
+
+articles = soup.find_all("div", {"data-testid" :"card-text-wrapper"})
+
+for article in articles:
+    headline_tag = article.find("h2", {"data-testid":"card-headline"})
+
+    if not headline_tag:
+        continue
+
+    description_tag = article.find("p", {"data-testid":"card-description"})
+
+    headline = headline_tag.get_text()
+    description = description_tag.get_text() if description_tag else "-"
+
+    data["Headline"].append(headline)
+    data["Description"].append(description)
+
+df = pd.DataFrame.from_dict(data)
+save_path = save_path = r"C:\Desktop\Data Extraction\BBC News Headlines\News\Latin America\Latin America latest updates.xlsx"
+
+df.to_excel(save_path, index=False)
